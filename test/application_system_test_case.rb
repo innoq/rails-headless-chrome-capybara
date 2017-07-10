0fb6f4041d8bb181e1157f9d6ff4b5ca52504058
Robert Glaser <mail@robert-glaser.de>
2017-07-10 11:30:42 +0200

    Restructure stuff a bit

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,17 +1,4 @@
 require "test_helper"
-require "selenium/webdriver"
-
-Capybara.register_driver(:headless_chrome) do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu] }
-  )
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-  )
-end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :headless_chrome
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,18 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu] }
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+  )
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
